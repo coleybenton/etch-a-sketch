1f10a0926e8d1142444d8bfdebdf4865ecbc55be
Allah Benton <alberaco99@gmail.com>
2023-01-17 19:33:09 -0500

    Fixed div grid, added mouseover event listeners to script.js

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
-/* 
+const container = document.querySelector("#container");
 
-  - Create 16x16 grid of divs within "container" div
-  - idea
-    - create an array
-    - loop values in there up to 255
-    - reference the array values in a function
-    - assign the parameter as variable to increment
-
-*/
-
-// let gridTotal = prompt(number) then number x number
-const containerDiv = document.querySelector("#container");
-const varArray = [];
-
-let i = 0;
-while (i < 255) {
-  varArray[i] = `div${i}`;
-  i++;
+function makeRows(rows, columns) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-columns', columns);
+  for (let i = 0; i < (rows * columns); i++) {
+    let cell = document.createElement("div");
+    cell.textContent = (i + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
 };
 
-function createDivFromArray(arrayValue) {
-  arrayValue = document.createElement("div");
-  containerDiv.appendChild(arrayValue);
+makeRows(16,16);
+
+const gridItems = document.querySelectorAll(".grid-item");
+
+for (let i = 0; i < (gridItems.length); i++) {	
+  gridItems[i].addEventListener("mouseover", function (e) { 
+  gridItems[i].classList.toggle("black-background");
+    });
 };
-
-for (let i = 0; i < 255; i++) {
-  createDivFromArray(varArray[i]);
-};
-
-const startingPointDiv = containerDiv.firstElementChild;
-
-for (let i = 0; i < 255; i++) {
-  let targetDiv = startingPointDiv.nextElementSibling;
-}
