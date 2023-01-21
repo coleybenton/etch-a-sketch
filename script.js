37b601dcaa2a29c78eb7d5528b91e6c78dd804c9
Allah Benton <alberaco99@gmail.com>
2023-01-21 02:45:40 -0500

    Finished!

--- script.js
+++ script.js
@@ -5,17 +5,46 @@ function makeRows(rows, columns) {
   container.style.setProperty('--grid-columns', columns);
   for (let i = 0; i < (rows * columns); i++) {
     let cell = document.createElement("div");
-    cell.textContent = (i + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16,16);
+const promptButton = document.querySelector("#prompt-button");
+const blackButton = document.querySelector("#black-button");
+const rainbowButton = document.querySelector("#rainbow-button");
+makeRows(16, 16);
+
+function createGridPrompt() {
+  container.textContent = "";
+  let gridNumber = prompt("Number of squares per side? Under 100 please");
+  if (gridNumber > 100) {
+    alert("Please keep it below 100");
+  } else {
+      makeRows(gridNumber, gridNumber);
+  };
+};
 
 const gridItems = document.querySelectorAll(".grid-item");
+const background = document.querySelector(".background");
 
-for (let i = 0; i < (gridItems.length); i++) {	
+blackButton.addEventListener("click", function (e) {
+ for (let i = 0; i < gridItems.length; i++) {	
   gridItems[i].addEventListener("mouseover", function (e) { 
-  gridItems[i].classList.toggle("black-background");
+    gridItems[i].classList.add("background");
     });
-};
+  };
+});
+
+rainbowButton.addEventListener("mouseover", function (e) {
+  for(let i = 0; i < gridItems.length; i++) {
+    let rgbGenOne = Math.floor(Math.random() * 255);
+    let rgbGenTwo = Math.floor(Math.random() * 255);
+    let rgbGenThree = Math.floor(Math.random() * 255);
+    gridItems[i].addEventListener("mouseover", function (e) {
+      gridItems[i].style.cssText = `background-color: rgb(${rgbGenOne},${rgbGenTwo},${rgbGenThree});`;
+    });
+  };
+});
+
+promptButton.addEventListener("click", createGridPrompt);
+
